c583ec1e67b5aa0ab51fa2c8c2a5e90520e968d3
Deneshwar Bhandarkar <deneshwarbhandarkar@gmail.com>
2024-08-07 09:51:36 +0530

    removed unwanted code

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,19 @@ import Navbar from "./components/Navbar/Navbar";
 import Signup from "./pages/Signup";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
-import Home from "./pages/Home.jsx"
+import Home from "./pages/Home.jsx";
 
 function App() {
   return (
-    <><div></div>
+    <>
       <Navbar />
       <Footer />
       <Routes>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
-        <Route path="/about" element={<About/>}></Route>
-          <Route path="/Home" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/Home" element={<Home />}></Route>
       </Routes>
     </>
   );
